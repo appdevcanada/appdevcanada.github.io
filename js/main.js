560f93d6e92e980b649c90d6a27fb337c6fa94b3
Luis Souza <appdevcanada@gmail.com>
2020-12-08 10:45:01 -0500

    visual reno | more projects info | minor bugs fix

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+'use strict'
+
+var slideIndex = 1;
+
 function loadImage(id, targetId) {
   var el = document.getElementById(id);
   var targetEl = targetId ? document.getElementById(targetId) : el;
@@ -19,13 +23,47 @@ function loadImage(id, targetId) {
 }
 
 function flippage() {
-  let fliped = document.getElementById('cardface').classList.contains('is-flipped');
+  let flipped = document.getElementById('cardface').classList.contains('is-flipped');
   document.getElementById('cardface').classList.toggle('is-flipped');
-  document.getElementById('btnFlip').textContent = `${fliped ? 'Show Skills' : 'Hide Skills'}`;
+  document.getElementById('btnFlip').textContent = `${flipped ? 'Show Skills' : 'Hide Skills'}`;
+  setTimeout(function () {
+    document.getElementsByClassName('slideshow-container')[0].style.display = `${flipped ? 'block' : 'none'}`;
+  }, 300)
+}
+
+function plusSlides(ev) {
+  showSlides(slideIndex += parseInt(ev.currentTarget.dataset.idx));
+}
+
+function currentSlide(ev) {
+  showSlides(slideIndex = parseInt(ev.target.dataset.idx));
+}
+
+function showSlides(sldIndex) {
+  console.log(sldIndex)
+  let dots = document.getElementsByClassName("dot");
+  let slides = document.getElementsByClassName("mySlides");
+  if (sldIndex > slides.length) { slideIndex = 1 }
+  if (sldIndex < 1) { slideIndex = slides.length }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].classList.remove("active");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].classList.toggle("active");
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   loadImage("wallpaper");
   loadImage("pictureImage", "picture");
-  document.getElementById('btnFlip').addEventListener('click', flippage)
+  document.getElementById('btnFlip').addEventListener('click', flippage);
+  let dot = document.getElementsByClassName("dot");
+  for (let i = 0; i < dot.length; i++) {
+    dot[i].addEventListener("click", currentSlide);
+  }
+  document.getElementById("prev").addEventListener('click', plusSlides);
+  document.getElementById("next").addEventListener('click', plusSlides);
+  showSlides(slideIndex);
 });
